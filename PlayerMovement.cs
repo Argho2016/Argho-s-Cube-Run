3d9bd5d5888c18048b6b05cb417e227eaeafb3d9
Argho2016 <aiub2016@yahoo.com>
2020-01-01 21:04:41 +0600

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewayForce = 500f;
+	
+	
+	// Update is called once per frame
+    //We used fixed update because this fuction is fixed for the entire game
+	void FixedUpdate () {
+        //Even out the frame rate for different pcs
+        //add forward force to the object
+        rb.AddForce(0,0,forwardForce * Time.deltaTime);
+
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(sidewayForce * Time.deltaTime ,0,0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
